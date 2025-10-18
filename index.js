f5c87f044b57c0f7504d36398929b065ac65ad34
Nusrat323 <samanusrat18@gmail.com>
2025-10-19 04:31:09 +1400

    add vercel.json

--- index.js
+++ index.js
@@ -116,10 +116,10 @@ app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-// Vercel expects export default
+
 export default app;
 
-// Optional: for local testing
+
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
