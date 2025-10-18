e23e5512d52c0c162220d35d2ee4c5ce18510250
Nusrat323 <samanusrat18@gmail.com>
2025-10-19 02:20:39 +1400

    redeploy

--- models/Photo.js
+++ models/Photo.js
@@ -1,13 +1,4 @@
 
-
-{/*import { getDb } from "../index.js";
-
-export const getPhotoCollection = () => {
-  const db = getDb();
-  return db.collection("photos");
-};*/}
-
-
 export const getPhotoCollection = (db) => db.collection("photos");
 
 
--- models/Video.js
+++ models/Video.js
@@ -1,15 +1,3 @@
-
-
-
-
-{/*import { getDb } from "../index.js";
-
-export const getVideoCollection = () => {
-  const db = getDb();
-  return db.collection("videos");
-};*/}
-
-
 export const getVideoCollection = (db) => db.collection("videos");
 
 
